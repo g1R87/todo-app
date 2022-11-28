84dadba2b8ae711eff8db8a039d4fe3891fd0685
Prashant <prashantacharya@lftechnology.com>
2022-11-28 19:50:58 +0545

    Code of first class

---
+++ basics/script.js
@@ -0,0 +1,3 @@
+console.log('Hello World');
+
+console.log("How's it going?");
---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+let name = 'Prashant';
+
+let message = 'Hello ' + name + '!';
+
+const PI = 3.14;
+const LARGEST_CONTINENT = 'Asia';
+
+name = 123;
+
+console.log(name);
+console.log(message);
+console.log(PI);
+console.log(LARGEST_CONTINENT);
+
+console.log(typeof name);
