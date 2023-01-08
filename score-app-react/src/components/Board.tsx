1a88fe78057ffc463d868ea69caedbd8e6b58424
edr3x <dhunganaannanta@gmail.com>
2023-01-08 22:01:20 +0545

    score app react project init

---
+++ score-app-react/src/components/Board.tsx
@@ -0,0 +1,28 @@
+import { ScoreObject } from "../App";
+import ScoreCard from "./ScoreCard";
+
+function Board({
+  title,
+  matchDay,
+  scores,
+}: {
+  title: string;
+  matchDay: number;
+  scores: ScoreObject[];
+}) {
+  return (
+    <div className="board">
+      <div className="title">
+        <h1>{title}</h1>
+        <h3>Matchday {matchDay} of 38</h3>
+      </div>
+      <div className="scorecard-container">
+        {scores.map((team) => (
+          <ScoreCard date={team.date} team1={team.team1} team2={team.team2} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Board;
